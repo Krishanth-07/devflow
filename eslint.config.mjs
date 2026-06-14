62b45f0d3fc3eae784882dc7319949a1a7ce283a
Krishanth-07 <krishanth.072280@gmail.com>
2026-06-14 13:50:54 +0530

    Intial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,33 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import stylistic from '@stylistic/eslint-plugin'
+//import tailwind from 'eslint-plugin-tailwindcss'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  //...tailwind.configs['flat/recommended'],
 
-export default eslintConfig;
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+
+    rules: {
+      // JavaScript Standard Style-like rules
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+      'comma-dangle': ['error', 'never'],
+      'space-before-function-paren': ['error', 'always'],
+
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }]
+    }
+  },
+
+  eslintConfigPrettier,
+
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts'])
+])
